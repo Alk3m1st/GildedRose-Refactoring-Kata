1280907d28780682037aa8e07688356fa21c9fb4
Allan Oliveira <r9oliveira@gmail.com>
2020-02-01 17:16:34 +0800

    Added test for Item ToString() method.

---
+++ csharp/ItemTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace csharp
+{
+    [TestFixture]
+    public class ItemTest
+    {
+        [Test]
+        public void toString()
+        {
+            var item = new Item { Name = "name", Quality = 10, SellIn = 20 };
+
+            var expectedOutput = "name, 20, 10";
+
+            Assert.AreEqual(expectedOutput, item.ToString());
+        }
+    }
+}
