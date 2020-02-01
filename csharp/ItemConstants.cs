f5576f0fbc8383d91f726e062202e5e4e0e22a2d
Allan Oliveira <r9oliveira@gmail.com>
2020-02-01 23:43:31 +0800

    Made ItemConstants actually constant...

--- csharp/ItemConstants.cs
+++ csharp/ItemConstants.cs
@@ -8,8 +8,8 @@ namespace csharp
 {
     public static class ItemConstants
     {
-        public static readonly string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
-        public static readonly string AgedBrie = "Aged Brie";
-        public static readonly string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        public const string AgedBrie = "Aged Brie";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
     }
 }
