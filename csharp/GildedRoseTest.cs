6bec775485a7c1cb389255ff83d4845e9487c37f
Allan Oliveira <r9oliveira@gmail.com>
2020-02-01 21:51:49 +0800

    Some discovery and whitebox tests.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -6,13 +6,27 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
+        [TestCase("foo", 0, 0, "foo, -1, 0")]
+        [TestCase("Aged Brie", 0, 0, "Aged Brie, -1, 2")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 0, "Backstage passes to a TAFKAL80ETC concert, -1, 0")]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 0, "Sulfuras, Hand of Ragnaros, 0, 0")]
+        [TestCase("foo", 0, 25, "foo, -1, 23")]
+        [TestCase("Aged Brie", 0, 25, "Aged Brie, -1, 27")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 25, "Backstage passes to a TAFKAL80ETC concert, -1, 0")]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 25, "Sulfuras, Hand of Ragnaros, 0, 25")]
+        [TestCase("foo", 0, 60, "foo, -1, 58")]
+        [TestCase("Aged Brie", 0, 60, "Aged Brie, -1, 60")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 60, "Backstage passes to a TAFKAL80ETC concert, -1, 0")]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 60, "Sulfuras, Hand of Ragnaros, 0, 60")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 25, "Backstage passes to a TAFKAL80ETC concert, 4, 28")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 25, "Backstage passes to a TAFKAL80ETC concert, 9, 27")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 25, "Backstage passes to a TAFKAL80ETC concert, 14, 26")]
+        public void foo(string name, int sellIn, int quality, string expectedOutput)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual(expectedOutput, Items[0].ToString());
         }
     }
 }
